de952a551ca570dbaa2a64e6b6744abb9ea5dfdf
JASK <jskowron12@gmail.com>
2021-05-30 18:54:29 +0200

    Utworzenie projektu WebAPI

---
+++ RecruitmentServiceAPI/src/main/java/com/example/recruitmentserviceapi/RecruitmentServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.recruitmentserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmentServiceApiApplication.class, args);
+    }
+
+}
