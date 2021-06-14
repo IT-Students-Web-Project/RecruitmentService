2aa3c9cee9581247e8aa79b2a27d2b986c4e56cc
Radosław Loth <r.loth@icloud.com>
2021-06-14 22:07:03 +0200

    Update Person.java

--- RecruitmentServiceAPI/src/main/java/pl/recruitmentservice/restapi/model/Person.java
+++ RecruitmentServiceAPI/src/main/java/pl/recruitmentservice/restapi/model/Person.java
@@ -1,44 +1,93 @@
 package pl.recruitmentservice.restapi.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.Collections;
 import java.util.Objects;
 import java.util.List;
 
-@Getter
-@Setter
-@NoArgsConstructor
 @Entity
 @Table(name = "PERSONS", schema = "dbo", catalog = "RECRUITMENT")
 public class Person {
+    private Integer id;
+    private String lastName;
+    private String firstName;
+    private Address address;
+    private List<Order> orders;
+    private List<PersonsSkill> personSkills;
+
+    public Person() {
+
+    }
+
+    public Person(String lastName, String firstName, Address address) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.address = address;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
-    private Integer id;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     @Basic
     @Column(name = "LAST_NAME")
-    private String lastName;
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     @Basic
     @Column(name = "FIRST_NAME")
-    private String firstName;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ADDRESS_ID")
-    private Address address;
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "person")
-    private List<Order> orders;
+    public List<Order> getOrders() {
+        if(orders == null)
+            return Collections.emptyList();
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "person")
-    private List<PersonsSkill> personSkills;
+    public List<PersonsSkill> getPersonsSkills() {
+        if(personSkills == null)
+            return Collections.emptyList();
+        return personSkills;
+    }
+
+    public void setPersonsSkills(List<PersonsSkill> personSkills) {
+        this.personSkills = personSkills;
+    }
 
     @Override
     public boolean equals(Object o) {
