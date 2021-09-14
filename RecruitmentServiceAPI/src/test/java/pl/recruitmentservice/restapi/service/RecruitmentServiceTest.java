b8f3885a69ea003c961245401fc6b52344f8a86a
Jacek Barć <jacekbarc@outlook.com>
2021-09-15 00:12:13 +0200

    Added skills test, renamed tests

--- RecruitmentServiceAPI/src/test/java/pl/recruitmentservice/restapi/service/RecruitmentServiceTest.java
+++ RecruitmentServiceAPI/src/test/java/pl/recruitmentservice/restapi/service/RecruitmentServiceTest.java
@@ -10,7 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import pl.recruitmentservice.restapi.model.Address;
 import pl.recruitmentservice.restapi.model.Person;
+import pl.recruitmentservice.restapi.model.Skill;
 import pl.recruitmentservice.restapi.repository.PersonsRepository;
+import pl.recruitmentservice.restapi.repository.SkillRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,12 +24,23 @@ class RecruitmentServiceTest {
     @Autowired
     private PersonsRepository personsRepository;
 
+    @Autowired
+    private SkillRepository skillRepository;
+
     @Test
-    void dbTest(){
+    void foundAddedPerson_thenOk(){
         Address testAddress = new Address("testStreet",1,1,"testCity","testPC","testCountry");
         Person addedPerson = new Person("John","Doe", testAddress);
         personsRepository.save(addedPerson);
         Person foundPerson = personsRepository.getById(0);
         assertEquals("John", foundPerson.getFirstName());
+    }
+
+    @Test
+    void foundAddedSkill_thenOk(){
+        Skill addedSkill = new Skill("testSkill");
+        skillRepository.save(addedSkill);
+        Skill foundSkill = skillRepository.getById(0);
+        assertEquals("testSkill", foundSkill.getName());
     }
 }
