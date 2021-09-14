6fcdd9f3f230ee71ae9b1603f94083ac018f8566
Jacek Barć <jacekbarc@outlook.com>
2021-09-15 00:00:56 +0200

    Added dbTest

--- RecruitmentServiceAPI/src/test/java/pl/recruitmentservice/restapi/service/RecruitmentServiceTest.java
+++ RecruitmentServiceAPI/src/test/java/pl/recruitmentservice/restapi/service/RecruitmentServiceTest.java
@@ -1,9 +1,33 @@
 package pl.recruitmentservice.restapi.service;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.parameters.P;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import pl.recruitmentservice.restapi.model.Address;
+import pl.recruitmentservice.restapi.model.Person;
+import pl.recruitmentservice.restapi.repository.PersonsRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@SpringBootTest
 class RecruitmentServiceTest {
+
+    @Autowired
+    private PersonsRepository personsRepository;
+
+    @Test
+    void dbTest(){
+        Address testAddress = new Address("testStreet",1,1,"testCity","testPC","testCountry");
+        Person addedPerson = new Person("John","Doe", testAddress);
+        personsRepository.save(addedPerson);
+        Person foundPerson = personsRepository.getById(0);
+        assertEquals("John", foundPerson.getFirstName());
+    }
 }
