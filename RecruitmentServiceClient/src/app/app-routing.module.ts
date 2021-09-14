7ffe51949e3dc489e05b0a30844eee8983ef0c7c
Radosław Loth <r.loth@icloud.com>
2021-09-14 15:20:01 +0200

    Dodano formularz dodawania kandydata

--- RecruitmentServiceClient/src/app/app-routing.module.ts
+++ RecruitmentServiceClient/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './component/app.component';
 import { PersonComponent } from './component/person/person.component';
 import { SkillComponent } from './component/skill/skill.component';
-import {HomeComponent} from './home/home.component';
-import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './component/home/home.component';
+import {LoginComponent} from './component/login/login.component';
+import {AddCandidateComponent} from './component/add-candidate/add-candidate.component';
+import {AddProjectComponent} from './component/add-project/add-project.component';
+import {AddClientComponent} from './component/add-client/add-client.component';
 
 const routes: Routes = [
   {
@@ -15,6 +17,15 @@ const routes: Routes = [
   },
   {
     path: 'login', component: LoginComponent
+  },
+  {
+    path: 'addCandidate', component: AddCandidateComponent
+  },
+  {
+    path: 'addProject', component: AddProjectComponent
+  },
+  {
+    path: 'addClient', component: AddClientComponent
   },
   {
     path: 'person', children: [
--- RecruitmentServiceClient/src/app/app.module.ts
+++ RecruitmentServiceClient/src/app/app.module.ts
@@ -8,9 +8,12 @@ import { SkillComponent } from './component/skill/skill.component';
 import { LevelComponent } from './component/level/level.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MainComponent } from './component/main/main.component';
-import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { AddCandidateComponent } from './component/add-candidate/add-candidate.component';
+import { AddProjectComponent } from './component/add-project/add-project.component';
+import { AddClientComponent } from './component/add-client/add-client.component';
 
 @NgModule({
   declarations: [
@@ -20,14 +23,18 @@ import { LoginComponent } from './login/login.component';
     LevelComponent,
     MainComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    AddCandidateComponent,
+    AddProjectComponent,
+    AddClientComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
